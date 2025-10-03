7811b1edd59bcf3353abcc36eb4eb5bdc831c69f
Frederikmoeller <fmalle20@student.aau.dk>
2025-10-03 15:00:51 +0200

    Camera view to image
    - Added an extra camera to use for taking snapshots
    - Camera view now renders to an image
    - image is saved both on pc and in memory

---
+++ UnityProject/Assets/Scripts/ImageProcessing/ImageProcessing.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ImageProcessing : MonoBehaviour
+{
+    [SerializeField] private ViewToImage _viewToImage;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/Scripts/ImageProcessing/ViewToImage.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using UnityEngine;
+using File = UnityEngine.Windows.File;
+using Input = UnityEngine.Input;
+
+public class ViewToImage : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    public Texture2D CameraViewImage;
+    [SerializeField] private int width = 1920;
+    [SerializeField] private int height = 1080;
+    [SerializeField] private string path = "C:/Users/byacr/Pictures";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _camera.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        if (Input.GetKeyDown(KeyCode.F9))
+        {
+            CameraViewToImage();
+        }
+    }
+
+    private void CameraViewToImage()
+    {
+        // Create a temporary RenderTexture
+        RenderTexture rt = new RenderTexture(width, height, 24);
+        _camera.targetTexture = rt;
+
+        // Render the camera’s view
+        _camera.Render();
+
+        // Activate RenderTexture and read pixels into Texture2D
+        RenderTexture.active = rt;
+        CameraViewImage = new Texture2D(width, height, TextureFormat.RGB24, false);
+        CameraViewImage.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        CameraViewImage.Apply();
+
+        // Reset
+        _camera.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(rt);
+        
+        byte[] bytes = CameraViewImage.EncodeToPNG();
+        path = Path.Combine(path, "CameraView.png");
+        File.WriteAllBytes(path, bytes);
+    }
+}
