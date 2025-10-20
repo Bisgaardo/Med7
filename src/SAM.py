448004e63e47f1ec298532e49d2e6be8b28a2f79
Katrine <katrine.moerchsabra@gmail.com>
2025-10-20 14:08:43 +0200

    Added SAM
    - Added IPYNB file with SAM model
    - Added necessary libraries to run model

--- src/SAM.py
+++ src/SAM.py
@@ -1,2 +1,18 @@
-# Supposedly the script to run SAM
-print("Hello world! From Python")
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+from segment_anything import sam_model_registry, SamPredictor
+
+
+sam = sam_model_registry["default"](checkpoint=r"C:\Users\katri\University\Semester 1\Projekt\Files\sam_vit_h_4b8939.pth")
+predictor = SamPredictor(sam)
+
+image = cv2.imread(r"C:\Users\katri\Pictures\Work\Truck.png")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+predictor.set_image(image)
+masks, _, _ = predictor.predict()
+
+
